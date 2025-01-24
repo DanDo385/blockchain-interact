6439e6e1a550d63c21721397af437c7408faffe1
Daniel Magro <dando385@yahoo.com>
2025-01-23 21:13:54 -0500

    add smart contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID", // Replace with your Infura URL
+      accounts: [`0x${YOUR_PRIVATE_KEY}`] // Replace with your wallet private key
+    },
+  },
 };
