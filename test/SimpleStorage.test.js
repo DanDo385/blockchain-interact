64d84f5f2b911f51bf584c01ae733d9103b9f188
Daniel Magro <dando385@yahoo.com>
2025-01-28 19:16:30 -0500

    testing branches

--- test/SimpleStorage.test.js
+++ test/SimpleStorage.test.js
@@ -14,7 +14,16 @@ describe("SimpleStorage", function () {
 
     describe("Saving data", function () {
         it("Should save name correctly", async function () {
-            await simpleStorage.saveName("Test Name", 100);
+            await simpleStorage.saveName("Test Name");
+            const block = await simpleStorage.getBlock(0);
+            
+            expect(block.name).to.equal("Test Name");
+            expect(block.sum).to.equal(0);
+            expect(block.creator).to.equal(owner.address);
+        });
+
+        it("Should save name and sum correctly", async function () {
+            await simpleStorage.saveNameAndSum("Test Name", 100);
             const block = await simpleStorage.getBlock(0);
             
             expect(block.name).to.equal("Test Name");
@@ -29,6 +38,27 @@ describe("SimpleStorage", function () {
             expect(block.name).to.equal("");
             expect(block.sum).to.equal(100);
             expect(block.creator).to.equal(owner.address);
+        });
+
+        it("Should increment block count correctly", async function () {
+            await simpleStorage.saveName("First");
+            await simpleStorage.saveNameAndSum("Second", 100);
+            await simpleStorage.saveSum(50, 50);
+
+            expect(await simpleStorage.blockCount()).to.equal(3);
+        });
+
+        it("Should emit BlockCreated event", async function () {
+            await expect(simpleStorage.saveNameAndSum("Test Name", 100))
+                .to.emit(simpleStorage, "BlockCreated")
+                .withArgs(0, "Test Name", 100, owner.address);
+        });
+    });
+
+    describe("Getting data", function () {
+        it("Should revert when accessing invalid block ID", async function () {
+            await expect(simpleStorage.getBlock(0))
+                .to.be.revertedWith("Invalid block ID");
         });
     });
 }); 
